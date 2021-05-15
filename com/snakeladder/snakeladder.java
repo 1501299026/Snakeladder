700ac26e7f55b21564371c2c95601f1e34dd176f
1501299026 <sinumsd98@gmail.com>
2021-05-15 11:19:57 -0700

    ADDED: Introduciong one player added

--- com/snakeladder/snakeladder.java
+++ com/snakeladder/snakeladder.java
@@ -5,6 +5,7 @@ public class snakeladder {
 	public static void main(String[] args) {
 		
        System.out.println("welcome to snake and ladder stimulation");
+       int player1Position=0;
 	}
 
 }
