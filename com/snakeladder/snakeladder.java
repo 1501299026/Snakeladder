86ca6c84e52473459d17ddb3b4953d518b1dc107
1501299026 <sinumsd98@gmail.com>
2021-05-15 21:18:53 -0700

    ADDED: Check for move added

--- com/snakeladder/snakeladder.java
+++ com/snakeladder/snakeladder.java
@@ -1,13 +1,26 @@
 package com.snakeladder;
 
 public class snakeladder {
+	public static final int NO_MOVE=0,LADDER=1,SNAKE=2;
 
 	public static void main(String[] args) {
 		
        System.out.println("welcome to snake and ladder stimulation");
        int player1Position=0;
-       int dies =(int) Math.floor(Math.random()*10)%6+1;
-       System.out.println("Dies"+dies);
+
+		int dice= (int) Math.floor(Math.random()*10)%6 +1;
+
+		int move= (int) Math.floor(Math.random()*10)%3;
+		
+		switch (move)
+		{
+			case LADDER: 
+						player1Position+=dice;
+						break;
+			case SNAKE:
+						player1Position-=dice;
+						break;	
 	}
 
+	}
 }
