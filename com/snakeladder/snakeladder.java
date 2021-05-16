544f67498d4cb1c723b32c81d75b3ad65b8d8a05
1501299026 <sinumsd98@gmail.com>
2021-05-15 20:28:06 -0700

    ADDED Rolling dies added

--- com/snakeladder/snakeladder.java
+++ com/snakeladder/snakeladder.java
@@ -6,6 +6,8 @@ public class snakeladder {
 		
        System.out.println("welcome to snake and ladder stimulation");
        int player1Position=0;
+       int dies =(int) Math.floor(Math.random()*10)%6+1;
+       System.out.println("Dies"+dies);
 	}
 
 }
